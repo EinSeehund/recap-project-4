3984116e8ba68cb971cc295a0e2ecb69c823d25b
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-08-07 15:21:10 +0200

    Add cards with inital color array according to issue #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
 
 function App() {
+    const [colors, setColors] = useState(initialColors);
+
     return (
         <>
             <h1>Theme Creator</h1>
-            <p>display colors here</p>
+            {colors.map((color) => (
+                <Color
+                    key={color.id}
+                    color={color.hex}
+                    role={color.role}
+                    contrastText={color.contrastText}
+                />
+            ))}
         </>
     );
 }
--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,5 +1,13 @@
 import "./Color.css";
 
-export default function Color({ color }) {
-  return null;
+export default function Color({ color, role, contrastText }) {
+    return (
+        <article className="color-card" style={{ backgroundColor: color }}>
+            <h2 className="color-card-headline">{color}</h2>
+            <p style={{ color: contrastText }}>
+                <b>{role}</b>
+            </p>
+            <p style={{ color: contrastText }}>{`contrast: ${contrastText}`}</p>
+        </article>
+    );
 }
