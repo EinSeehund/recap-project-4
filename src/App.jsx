fc74701c4fd7f5916b904bf507d2a678bb545428
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-08-07 15:48:35 +0200

    Remove dead code and implement a variable for repeating font color

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from "react";
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
 
 function App() {
-    const [colors, setColors] = useState(initialColors);
-
     return (
         <>
             <h1>Theme Creator</h1>
-            {colors.map((color) => (
+            {initialColors.map((color) => (
                 <Color
                     key={color.id}
                     color={color.hex}
--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,13 +1,15 @@
 import "./Color.css";
 
 export default function Color({ color, role, contrastText }) {
+    const contrastStyle = { color: contrastText };
+
     return (
         <article className="color-card" style={{ backgroundColor: color }}>
             <h2 className="color-card-headline">{color}</h2>
-            <p style={{ color: contrastText }}>
+            <p style={contrastStyle}>
                 <b>{role}</b>
             </p>
-            <p style={{ color: contrastText }}>{`contrast: ${contrastText}`}</p>
+            <p style={contrastStyle}>{`contrast: ${contrastText}`}</p>
         </article>
     );
 }
