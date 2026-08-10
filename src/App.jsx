242d91a72ab3f4b1290d6d3f8a63a5cd46772430
EinSeehund <oleg.tjulenev@gmail.com>
2026-08-10 10:14:01 +0200

    Update src/App.jsx
    Co-authored-by: FelineHuhn <112863528+FelineHuhn@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     }
 
     function handleDeleteColor(colorId) {
-        setColors(colors.filter((color) => color.id != colorId));
+        setColors(colors.filter((color) => color.id !== colorId));
     }
 
     return (
